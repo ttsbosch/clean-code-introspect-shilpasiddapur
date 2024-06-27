dfded424bf182127d6d0ae98ccbfcf1979875c79
shilpasiddapur <43199526+shilpasiddapur@users.noreply.github.com>
2024-06-27 15:05:54 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,8 +1,21 @@
 #include "StringCalculator.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
 
 int StringCalculator::add(string numbers){
-    if(numbers..empty()){
+    if(numbers.empty() || numbers == "0"){
          return 0;
+    }
+    else{
+        istringstream input(numbers);
+        string token;
+        int sum = 0;
+        while (getline(input, token, ',')) {
+            sum += std::stoi(token);
+        }
+        return sum;
     }
    
 }
