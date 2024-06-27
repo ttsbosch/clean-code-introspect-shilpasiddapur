f50e30061249db0fb9f5320037c1c7676904dcbc
shilpasiddapur <43199526+shilpasiddapur@users.noreply.github.com>
2024-06-27 17:28:14 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -5,28 +5,27 @@
 using namespace std;
 
 
-int addNumbers(const std::string& input) {
+int addNumbers(const std::string& input, const std::string& delimiters) {
     int sum = 0;
     std::istringstream ss(input);
     std::string token;
+    bool negativeNum = false;
 
     for (char c : input) {
-        sum += c - '0'; 
-    }
-
-    return sum;
-}
-
-std::string removeDelimiters(const std::string& input, const std::string& delimiters) {
-    std::string result;
-
-    for (char c : input) {
-        if (delimiters.find(c) == std::string::npos) {
-            result += c;
+        cout<<"char "<<c<<endl;
+        if(c == '-'){
+            negativeNum  = true;
+        }
+        else if (delimiters.find(c) == std::string::npos) {
+             if(negativeNum){
+                 negativeNum = false;
+                 continue;
+             }
+              sum += c - '0';
+              cout<<"sum = "<<sum<<endl;
         }
     }
-
-    return result;
+    return sum;
 }
 
 int StringCalculator::add(string numbers){
@@ -35,8 +34,7 @@ int StringCalculator::add(string numbers){
     }
     else{
         std::string delimiters = ",;: \t\n/";
-        string result = removeDelimiters(numbers,delimiters);
-        int sum = addNumbers(result);
+        int sum = addNumbers(result,delimiters);
         return sum;
     }
    
